15fb83fcf2b12fa3b09e53d1f2d06bcaa13084ac
JmZeroQAQ <1932699628@qq.com>
2024-08-23 09:44:31 +0800

    Fix: SiteFooter CSS style

--- src/components/SiteFooter.vue
+++ src/components/SiteFooter.vue
@@ -2,11 +2,14 @@
   <div class="bg-white dark:bg-slate-800 p-10 w-full flex justify-center mt-20">
     <div>
       <div class="text-xl font-bold text-center font-mono mb-2">凉界</div>
-      <div class="text-slate-500 dark:text-slate-400 text-center font-mono">
-        ©2023-2024 凉界版权所有 |
+      <div class="text-center font-mono">
+        <span class="text-slate-500 dark:text-slate-400">
+          ©2023-2024 凉界版权所有 |
+        </span>
         <a
-          class="text-sky-500 hover:text-sky-600 dark:text-slate-400"
+          class="text-sky-500 dark:hover:text-sky-400 hover:text-sky-600"
           href="https://beian.miit.gov.cn/"
+          target="_blank"
         >
           粤ICP备2024294384号-1
         </a>
